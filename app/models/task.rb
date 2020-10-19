58bf906221ba0601c6f976881374ae8b19be3265
Sam Naish <sam.naish92@googlemail.com>
2020-10-19 18:31:21 +0100

    added styles to edit new pages for projects and tasks

--- app/models/task.rb
+++ app/models/task.rb
@@ -8,4 +8,16 @@ class Task < ApplicationRecord
     ['In progress', 'in-progress'], 
     ['Complete', 'complete']
   ]
+
+
+  def badge_colour
+    case status
+    when 'not-started'
+      'secondary'
+    when 'in-progress'
+        'info'
+    when 'complete'
+        'success'
+    end
+  end
 end
