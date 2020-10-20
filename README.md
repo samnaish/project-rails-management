94975fc1e514452c75ede91cb1139c47b9ffb0a4
Sam Naish <sam.naish92@googlemail.com>
2020-10-20 20:06:43 +0100

    feat: added drag and drop with sortablejs

--- README.md
+++ README.md
@@ -1,24 +1,15 @@
 # README
 
-This README would normally document whatever steps are necessary to get the
-application up and running.
-
-Things you may want to cover:
-
-* Ruby version
+* Ruby version = ruby '2.6.6'
 
 * System dependencies
 
+    - sortablejs
+    - devise
+    - nested_scaffold
+    - bootstrap
+
 * Configuration
 
-* Database creation
-
-* Database initialization
-
-* How to run the test suite
-
-* Services (job queues, cache servers, search engines, etc.)
-
-* Deployment instructions
-
-* ...
+    - clone the repo
+    -  run bundle install
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,8 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
+import Sortable from 'sortablejs';
+
 import "../stylesheets/application"
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
@@ -15,3 +17,10 @@ import "../stylesheets/application"
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+document.addEventListener('turbolinks:load', () => {
+    var element = document.getElementById('tasks-list');
+    var sortable = Sortable.create(element, {
+        animation: 150
+    })
+})
